789269c10b12d361e85e802bf0168a3ccd6218d4
Patrick Radtke <patrick@cirrusidentity.com>
2016-09-01 12:25:40 -0700

    Add sample attributemap for stormpath attributes

---
+++ attributemap/stormpath2oid.php
@@ -0,0 +1,8 @@
+<?php
+$attributemap = array(
+    'fullName'                      => 'urn:oid:2.16.840.1.113730.3.1.241',
+    'email'                         => 'urn:oid:0.9.2342.19200300.100.1.3',
+    'givenName'                     => 'urn:oid:2.5.4.42',
+    'surname'                       => 'urn:oid:2.5.4.4',
+    'username'                      => 'urn:oid:0.9.2342.19200300.100.1.1',
+);
